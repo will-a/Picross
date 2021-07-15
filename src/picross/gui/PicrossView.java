f7071691de11c6ac4b695edb22659232942f0ae0
will-a <willa@fastmail.com>
2021-07-15 08:20:54 -0400

    initial commit

---
+++ src/picross/gui/PicrossView.java
@@ -0,0 +1,11 @@
+package picross.gui;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class PicrossView extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+
+    }
+}
