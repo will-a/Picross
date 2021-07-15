7b271c1237b4f51269e86854cea06cde849e09ce
will-a <willa@fastmail.com>
2021-07-15 10:34:46 -0400

    implemented basic model class, initializer in view

--- src/picross/gui/PicrossView.java
+++ src/picross/gui/PicrossView.java
@@ -6,33 +6,51 @@ import javafx.scene.control.Button;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.TilePane;
 import javafx.stage.Stage;
+import picross.model.PicrossBoard;
+
+import java.util.List;
 
 public class PicrossView extends Application {
+    private PicrossBoard board;
+
+    @Override
+    public void init() throws Exception {
+        List<String> args = getParameters().getRaw();
+        board = new PicrossBoard(Integer.parseInt(args.get(0)), Integer.parseInt(args.get(1)));
+    }
+
     @Override
     public void start(Stage stage) throws Exception {
         BorderPane scenePane = new BorderPane();
 
-        TilePane picrossBoard = new TilePane();
-        picrossBoard.setPrefRows(10);
-        picrossBoard.setPrefColumns(10);
+        TilePane picrossGrid = new TilePane();
+        picrossGrid.setPrefRows(board.getNumRows());
+        picrossGrid.setPrefColumns(board.getNumCols());
 
-        Button[][] buttons = new Button[10][10];
-        for (int row = 0; row < buttons.length; row++) {
-            for (int col = 0; col < buttons[0].length; col++) {
+        for (int row = 0; row < board.getNumRows(); row++) {
+            for (int col = 0; col < board.getNumCols(); col++) {
                 Button b = new Button();
                 b.setPrefHeight(50);
                 b.setPrefWidth(50);
                 b.setOnAction(e -> {
-                    System.out.println(picrossBoard.getChildren().indexOf(b));
+                    System.out.println(picrossGrid.getChildren().indexOf(b));
                 });
-                picrossBoard.getChildren().add(b);
+                picrossGrid.getChildren().add(b);
             }
         }
-        scenePane.setCenter(picrossBoard);
+        scenePane.setCenter(picrossGrid);
 
         Scene scene = new Scene(scenePane);
         stage.setScene(scene);
         stage.setTitle("Picross");
         stage.show();
+    }
+
+    public static void main(String[] args) {
+        if (args.length != 2) {
+            System.err.println("Program requires <rowNum> and <colNum> arguments");
+        } else {
+            Application.launch(args);
+        }
     }
 }
--- src/picross/model/PicrossBoard.java
+++ src/picross/model/PicrossBoard.java
@@ -1,4 +1,24 @@
 package picross.model;
 
 public class PicrossBoard {
+    private int numRows;
+    private int numCols;
+
+    public PicrossBoard() {
+        numRows = 10;
+        numCols = 10;
+    }
+
+    public PicrossBoard(int rows, int cols) {
+        numRows = rows;
+        numCols = cols;
+    }
+
+    public int getNumRows() {
+        return numRows;
+    }
+
+    public int getNumCols() {
+        return numCols;
+    }
 }
