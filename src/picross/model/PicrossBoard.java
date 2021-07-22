0c2a569596ab3504fb990a26b0fc9cbe8e0004ca
will-a <willa@fastmail.com>
2021-07-21 20:08:02 -0400

    fixed 2d array malarchy

--- src/picross/model/PicrossBoard.java
+++ src/picross/model/PicrossBoard.java
@@ -12,12 +12,10 @@ public class PicrossBoard extends GridPane {
     private static double selectChance = 0.5;
     private int numRows;
     private int numCols;
-    private ArrayList<ArrayList<PicrossHint>> rowHintVals;
-    private ArrayList<ArrayList<PicrossHint>> colHintVals;
 
     private PicrossTile[][] board;
-    private VBox[] rowHints;
-    private HBox[] colHints;
+    private HBox[] rowHints;
+    private VBox[] colHints;
     private boolean[][] solution;
 
     public PicrossBoard(int rows, int cols) {
@@ -25,29 +23,29 @@ public class PicrossBoard extends GridPane {
         numCols = cols;
 
         board = new PicrossTile[numRows][numCols];
-        rowHints = new VBox[numCols];
-        colHints = new HBox[numRows];
+        rowHints = new HBox[numCols];
+        colHints = new VBox[numRows];
         solution = new boolean[numRows][numCols];
 
-        // top hint row
+        // left hint row
         for (int i = 0; i < numCols; i++) {
-            VBox v = new VBox();
-            v.setAlignment(Pos.BOTTOM_CENTER);
-            v.setPadding(new Insets(0, 0, 3, 0));
-            add(v, i + 1, 0);
+            HBox v = new HBox(2);
+            v.setAlignment(Pos.CENTER_RIGHT);
+            v.setPadding(new Insets(0, 5, 0, 0));
+            add(v, 0, i + 1);
             rowHints[i] = v;
         }
-        // left hint row
+        // top hint row
         for (int i = 0; i < numRows; i++) {
-            HBox h = new HBox(2);
-            h.setAlignment(Pos.CENTER_RIGHT);
-            h.setPadding(new Insets(0, 5, 0, 0));
-            add(h, 0, i + 1);
+            VBox h = new VBox();
+            h.setAlignment(Pos.BOTTOM_CENTER);
+            h.setPadding(new Insets(0, 0, 3, 0));
+            add(h, i + 1, 0);
             colHints[i] = h;
         }
 
-        for (int col = 1; col <= numRows; col++) {
-            for (int row = 1; row <= numCols; row++) {
+        for (int row = 1; row <= numRows; row++) {
+            for (int col = 1; col <= numCols; col++) {
                 PicrossTile b = new PicrossTile(row-1, col-1);
                 b.setPrefHeight(50);
                 b.setPrefWidth(50);
@@ -58,10 +56,10 @@ public class PicrossBoard extends GridPane {
     }
 
     public void newGame() {
-        rowHintVals = new ArrayList<>();
-        for (int i = 0; i < numRows; i++) rowHintVals.add(new ArrayList<>());
-        colHintVals = new ArrayList<>();
-        for (int i = 0; i < numCols; i++) colHintVals.add(new ArrayList<>());
+        ArrayList<ArrayList<PicrossHint>> rowHintVals = new ArrayList<>();
+        for (int i = 0; i < numCols; i++) rowHintVals.add(new ArrayList<>());
+        ArrayList<ArrayList<PicrossHint>> colHintVals = new ArrayList<>();
+        for (int i = 0; i < numRows; i++) colHintVals.add(new ArrayList<>());
         int[] rowCounts = new int[numCols];
         int colCounter = 0;
 
@@ -91,7 +89,7 @@ public class PicrossBoard extends GridPane {
             }
         }
 
-        for (int row = 0; row < numRows; row++) {
+        for (int row = 0; row < numCols; row++) {
             if (rowCounts[row] > 0) rowHintVals.get(row).add(new PicrossHint(rowCounts[row]));
             rowHints[row].getChildren().clear();
             for (PicrossHint ph : rowHintVals.get(row)) {
@@ -99,20 +97,18 @@ public class PicrossBoard extends GridPane {
             }
         }
 
-        for (int col = 0; col < numCols; col++) {
+        for (int col = 0; col < numRows; col++) {
             colHints[col].getChildren().clear();
             for (PicrossHint ph : colHintVals.get(col)) {
                 colHints[col].getChildren().add(ph);
             }
         }
-
-        print();
     }
 
     public boolean checkSolution() {
         for (int row = 0; row < numRows; row++) {
             for (int col = 0; col < numCols; col++) {
-                if (board[row][col].isSelected() != solution[col][row]) {
+                if (board[row][col].isSelected() != solution[row][col]) {
                     return false;
                 }
             }
